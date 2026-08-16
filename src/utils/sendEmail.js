3565e37baaa326d3fe0e29a9ce54c2dc25ba451d
Ali Hassan <itx.alihassanch.com>
2026-08-16 23:59:46 +0500

    function to send email is created

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,9 +1,17 @@
-const {Resend} = require('resend')
+const { Resend } = require('resend')
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-const sendEmail = async (to, subject,html) => {
-    const {data,error} = await resend.emails.send({
-        from
+const sendEmail = async (to, subject, html) => {
+    const { data, error } = await resend.emails.send({
+        from: process.env.RESEND_EMAIL,
+        to,
+        subject,
+        html
     })
+    if (error) throw new Error(error.message)
+
+    return data
 } 
+
+module.exports = sendEmail
