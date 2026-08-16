8e84aa451b961a4c9ccabd72c9244f278882d2f0
Ali Hassan <itx.alihassanch.com>
2026-08-16 23:55:59 +0500

    creating the function to send the email to users using the Resend

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,9 @@
+const {Resend} = require('resend')
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+const sendEmail = async (to, subject,html) => {
+    const {data,error} = await resend.emails.send({
+        from
+    })
+} 
