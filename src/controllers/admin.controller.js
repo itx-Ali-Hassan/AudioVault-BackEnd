c7e9ae4a8bb6ed194cffac01c226762b9598a01a
Ali Hassan <itx.alihassanch.com>
2026-08-18 00:21:36 +0500

    create the function to getAllUsers in admin.controller.js file

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,4 +1,15 @@
-const getAllUsers = async (req, res) => { }
+const userModel = require('../models/user.model')
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await userModel.find().select('-password')
+
+        res.status(200).json({ message: 'All users fetched successfully', users })
+    } catch (error) {
+        console.log('Got an error while fetching the users', error.message)
+        res.status(500).json({ message: 'Got an error while fetching the users' })
+    }
+}
 
 const deleteUser = async (req, res) => { }
 
